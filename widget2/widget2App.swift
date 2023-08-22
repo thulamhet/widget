02ab639442bcc7f317e29d3793a39c43b53ec8ea
Nguyen Cong Thu <congthu137@gmail.com>
2023-08-22 08:55:42 +0700

    Initial Commit

---
+++ widget2/widget2App.swift
@@ -0,0 +1,17 @@
+//
+//  widget2App.swift
+//  widget2
+//
+//  Created by Nguyễn Công Thư on 22/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct widget2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ widget2Tests/widget2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  widget2Tests.swift
+//  widget2Tests
+//
+//  Created by Nguyễn Công Thư on 22/08/2023.
+//
+
+import XCTest
+@testable import widget2
+
+final class widget2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
